5487403f673c3371dac27bb234098880f34b06c9
teguhrachmadi1206-png <teguh.rachmadi.1206@gmail.com>
2026-04-04 22:57:05 +0700

    feat: add gh-pages script

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/teguhrachmadi1206-png/Venue-Landing-Page",
   plugins: [react()],
 })
