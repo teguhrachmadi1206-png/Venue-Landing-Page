2135c71c1d65e2c06d2bd6f11759a8538a46aabc
teguhrachmadi1206-png <teguh.rachmadi.1206@gmail.com>
2026-04-04 23:12:20 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/teguhrachmadi1206-png/Venue-Landing-Page",
+  base: "/Venue-Landing-Page/",
   plugins: [react()],
   server: {
     proxy: {
