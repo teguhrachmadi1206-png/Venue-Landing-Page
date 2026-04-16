af1feecefc22ac8cbb0b7125916ea50beabbb18e
teguhrachmadi1206-png <teguh.rachmadi.1206@gmail.com>
2026-04-16 22:44:33 +0700

    feat: add purchase confirmation modal

--- src/components/CustomModal.jsx
+++ src/components/CustomModal.jsx
@@ -1,21 +1,24 @@
 import "../styles/CustomModal.css"
+import { FocusTrap } from "focus-trap-react"
 
-export default function CustomModal({ onConfirm, onClose, isOpen }) {
+export default function CustomModal({ content }) {
 
 
     return (
-        <dialog className="modal">
-            <div className="modal-overlay">
-                <div className="modal-content">
-                    <h2 className="modal-title">Confirmation</h2>
-                    <p className="modal-text">Are You Sure?</p>
-                    <div className="modal-actions">
-                        <button className="modal-btn" onClick={onConfirm}>Yes</button>
-                        <button className="modal-btn" onClick={onClose}>No</button>
+        <FocusTrap>
+            <div className="modal">
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <h2 className="modal-title">{content.title}</h2>
+                        <p className="modal-text">{content.message}</p>
+                        <div className="modal-actions">
+                            {content.yesBtn}
+                            {content.noBtn}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </dialog>
+            </div >
+        </FocusTrap>
     )
 }
 
